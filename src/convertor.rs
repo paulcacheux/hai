9e6839977b977d06643d8c53fff1f90214577ea8
Paul Cacheux <paulcacheux@gmail.com>
2018-11-21 21:55:45 +0100

    progress in ast2ir convertor

--- src/convertor.rs
+++ src/convertor.rs
@@ -49,13 +49,28 @@ impl Visitor for ProgramConvertor {
 
 #[derive(Debug, Clone)]
 struct FunctionConvertor {
+    sym_table: SymbolTable,
     builder: ir::builder::FunctionBuilder,
 }
 
 impl FunctionConvertor {
     pub fn new(name: &str, params: &[(String, ast::Type)]) -> Self {
-        let param_tys = params.iter().map(|p| ty_ast2ir(p.1.clone())).collect();
+        let mut param_tys = Vec::new();
+        for &(_, ref ty) in params {
+            param_tys.push(ty_ast2ir(ty.clone()));
+        }
+
+        let mut sym_table = SymbolTable::new();
+        sym_table.begin_scope(); // TODO
+        let mut builder = ir::builder::FunctionBuilder::new(name.to_string(), param_tys.clone());
+
+        for (index, &(ref param, _)) in params.iter().enumerate() {
+            let var = builder.create_variable_param(index);
+            sym_table.register_variable(param.to_string(), param_tys[index].clone(), var);
+        }
+
         FunctionConvertor {
+            sym_table: SymbolTable::new(),
             builder: ir::builder::FunctionBuilder::new(name.to_string(), param_tys),
         }
     }
@@ -66,5 +81,46 @@ fn ty_ast2ir(ty: ast::Type) -> ir::Type {
         ast::Type::Unit => ir::Type::Unit,
         ast::Type::Int => ir::Type::Int,
         ast::Type::Boolean => ir::Type::Boolean,
+    }
+}
+
+#[derive(Debug, Clone)]
+struct SymbolTable {
+    scopes: Vec<HashMap<String, TypedVariable>>,
+}
+
+#[derive(Debug, Clone)]
+struct TypedVariable {
+    ty: ir::Type,
+    var: ir::Variable,
+}
+
+impl SymbolTable {
+    fn new() -> Self {
+        SymbolTable { scopes: Vec::new() }
+    }
+
+    fn begin_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    fn end_scope(&mut self) {
+        self.scopes.pop();
+    }
+
+    fn register_variable(&mut self, name: String, ty: ir::Type, var: ir::Variable) {
+        self.scopes
+            .last_mut()
+            .unwrap()
+            .insert(name, TypedVariable { ty, var });
+    }
+
+    fn lookup_variable(&mut self, name: &str) -> Option<TypedVariable> {
+        for scope in self.scopes.iter().rev() {
+            if let Some(tv) = scope.get(name) {
+                return Some(tv.clone());
+            }
+        }
+        None
     }
 }
